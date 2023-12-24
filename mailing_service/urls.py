1f9215745040deafd78f5387a127287d81e7d302
BalashovDV <korochun@yandex.ru>
2023-12-24 12:30:31 +0300

    Веб-сервис рассылок. Учеба в SKYPRO. Курсовой проект тема 6 Django. 1. Добавлено приложение для сервиса рассылок. 2. Созданы модели для классов рассылок, прописаны настройки и urls. 3. Из README.md убрана дублирующая информация. 4. Добавлен файл зависимостей.

---
+++ mailing_service/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from mailing_service.apps import MailingServiceConfig
+
+app_Name = MailingServiceConfig.name
+
+urlpatterns = [
+
+]
