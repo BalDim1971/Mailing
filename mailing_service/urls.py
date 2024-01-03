6a9faa7332261a085a391ca3ec847953f398b3ff
BalashovDV <korochun@yandex.ru>
2024-01-03 18:26:18 +0300

    Веб-сервис рассылок. Учеба в SKYPRO. Курсовой проект тема 6 Django. Очередная промежуточная версия. Заложен запуск нового клиента. Дописано меню и вынесено в отдельный файл.

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from config import settings
 from mailing_service.apps import MailingServiceConfig
-from mailing_service.views import ClientListView
+from mailing_service.views import ClientListView, ClientCreateView
 
 app_name = MailingServiceConfig.name
 
 
 urlpatterns = [
     path('', ClientListView.as_view(), name='client_list'),
+    path('client_create/', ClientCreateView.as_view(), name='client_create'),
 ]
--- mailing_service/views.py
+++ mailing_service/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy, reverse
 
 from mailing_service.models import Client
 
 
 class ClientListView(ListView):
     model = Client
+    extra_context = {
+        'title': 'Сервер почтовых рассылок'
+    }
+    
+    def get_queryset(self, *ard, **kwargs):
+        queryset = super().get_queryset(*ard, **kwargs)
+        
+        return queryset
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('last_name', 'first_name', 'patronymic',)
+    success_url = reverse_lazy('mailing_service:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.save()
+        return super().form_valid(form)
